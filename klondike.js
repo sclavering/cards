231e2cc13ae30ae418ce471c7c67a8f71eed98ca
Stephen Clavering <stephen@clavering.me.uk>
2004-03-21 20:34:50 +0000

    *** empty log message ***

--- klondike.js
+++ klondike.js
@@ -1,120 +1,119 @@
-var Klondike = new CardGame(CAN_TURN_STOCK_OVER);
+Games["Klondike"] = {
+  __proto__: BaseCardGame,
 
-Klondike.shortname = "klondike";
+  shortname: "klondike",
+  name: "Klondike",
 
-Klondike.init = function() {
-  this.sourceStacks = [this.waste].concat(this.stacks);
-};
-
-
-
-///////////////////////////////////////////////////////////
-//// start game
-Klondike.deal = function() {
-  var cards = this.shuffleDecks(1);
-  for(var i = 0; i < 7; i++) this.dealToStack(cards,this.stacks[i],i,1);
-  this.dealToStack(cards,this.stock,cards.length,0);
-};
+  canTurnStockOver: true,
 
 
-
-///////////////////////////////////////////////////////////
-//// Moving
-Klondike.canMoveToPile = function(card, stack) {
-  // last on stack must be opposite colour and consecutive to card, or stack empty and card is a king
-  var last = stack.lastChild;
-  return (last
-    ? (last.faceUp() && last.notSameColour(card) && last.isConsecutiveTo(card))
-    : card.isKing());
-};
+  init: function() {
+    this.sourceStacks = [this.waste].concat(this.stacks);
+  },
 
 
+  ///////////////////////////////////////////////////////////
+  //// start game
+  deal: function() {
+    var cards = this.shuffleDecks(1);
+    for(var i = 0; i < 7; i++) this.dealToStack(cards,this.stacks[i],i,1);
+    this.dealToStack(cards,this.stock,cards.length,0);
+  },
 
-///////////////////////////////////////////////////////////
-//// hint
-Klondike.getHints = function() {
-  this.getHintsForCard(this.waste.lastChild);
-  for(var i = 0; i < 7; i++) {
-    this.getHintsForCard(this.getLowestMoveableCard_AltColours(this.stacks[i]));
-  }
-};
-Klondike.getHintsForCard = function(card) {
-  if(!card) return;
-  var i, stack;
-  for(i = 0; i < 7; i++) {
-    stack = this.stacks[i];
-    if(this.canMoveTo(card,stack)) this.addHint(card,stack);
-  }
-  for(i = 0; i < 4; i++) {
-    stack = this.foundations[i];
-    if(this.canMoveTo(card,stack)) {
-      this.addHint(card,stack);
-      return; // don't hint more than one move to a foundation
+
+  ///////////////////////////////////////////////////////////
+  //// Moving
+  canMoveToPile: function(card, stack) {
+    // last on stack must be opposite colour and consecutive to card, or stack empty and card is a king
+    var last = stack.lastChild;
+    return (last
+      ? (last.faceUp() && last.notSameColour(card) && last.isConsecutiveTo(card))
+      : card.isKing());
+  },
+
+
+  ///////////////////////////////////////////////////////////
+  //// hint
+  getHints: function() {
+    this.getHintsForCard(this.waste.lastChild);
+    for(var i = 0; i < 7; i++) {
+      this.getHintsForCard(this.getLowestMoveableCard_AltColours(this.stacks[i]));
     }
+  },
+  getHintsForCard: function(card) {
+    if(!card) return;
+    var i, stack;
+    for(i = 0; i < 7; i++) {
+      stack = this.stacks[i];
+      if(this.canMoveTo(card,stack)) this.addHint(card,stack);
+    }
+    for(i = 0; i < 4; i++) {
+      stack = this.foundations[i];
+      if(this.canMoveTo(card,stack)) {
+        this.addHint(card,stack);
+        return; // don't hint more than one move to a foundation
+      }
+    }
+  },
+
+
+  ///////////////////////////////////////////////////////////
+  //// smart move
+  getBestMoveForCard: function(card) {
+    var i;
+    if(card.parentNode==this.waste) {
+      // hunt through tableau piles
+      for(i = 0; i < 7; i++)
+        if(this.canMoveToPile(card,this.stacks[i])) return this.stacks[i];
+    } else {
+      // find move to surrounding pile
+      var dest = this.searchAround(card,this.canMoveToPile);
+      if(dest) return dest;
+    }
+    // find move to foundation
+    for(i = 0; i < 4; i++) {
+      var stack = this.foundations[i];
+      if(stack==card.parentNode) continue;
+      if(this.canMoveToFoundation(card,stack)) return stack;
+    }
+    return null;
+  },
+
+
+  ///////////////////////////////////////////////////////////
+  //// Autoplay
+  autoplayMove: function() {
+    // automove cards to suit stacks
+    for(var i = 0; i < this.sourceStacks.length; i++) {
+      var last = this.sourceStacks[i].lastChild;
+      if(last && this.canAutoplayCard(last) && this.sendToFoundations(last)) return true;
+    }
+    return false;
+  },
+  // card can be autoplayed if both cards with the next lower number and of opposite colour are on foundations
+  canAutoplayCard: function(card) {
+    if(card.isAce() || card.number()==2) return true;
+    return (this.numCardsOnFoundations(card.altcolour(),card.number()-1) == 2);
+  },
+
+
+
+  ///////////////////////////////////////////////////////////
+  //// winning, scoring, undo
+  hasBeenWon: function() {
+    // game won if all 4 Foundations have 13 cards
+    for(var i = 0; i < 4; i++)
+      if(this.foundations[i].childNodes.length!=13)
+        return false;
+    return true;
+  },
+  getScoreForAction: function(action) {
+    return (
+      action=="move-to-foundation"   ?   10 :
+      action=="move-from-waste"      ?    5 :
+      action=="card-revealed"        ?    5 :
+      action=="move-from-foundation" ?  -15 :
+      action=="stock-turned-over"    ? -100 :
+      0);
   }
 };
-
-
-
-///////////////////////////////////////////////////////////
-//// smart move
-Klondike.getBestMoveForCard = function(card) {
-  var i;
-  if(card.parentNode==this.waste) {
-    // hunt through tableau piles
-    for(i = 0; i < 7; i++)
-      if(this.canMoveToPile(card,this.stacks[i])) return this.stacks[i];
-  } else {
-    // find move to surrounding pile
-    var dest = this.searchAround(card,Klondike.canMoveToPile);
-    if(dest) return dest;
-  }
-  // find move to foundation
-  for(i = 0; i < 4; i++) {
-    var stack = this.foundations[i];
-    if(stack==card.parentNode) continue;
-    if(this.canMoveToFoundation(card,stack)) return stack;
-  }
-  return null;
-};
-
-
-
-///////////////////////////////////////////////////////////
-//// Autoplay
-Klondike.autoplayMove = function() {
-  // automove cards to suit stacks
-  for(var i = 0; i < this.sourceStacks.length; i++) {
-    var last = this.sourceStacks[i].lastChild;
-    if(last && this.canAutoplayCard(last) && this.sendToFoundations(last)) return true;
-  }
-  return false;
-};
-// card can be autoplayed if both cards with the next lower number and of opposite colour are on foundations
-Klondike.canAutoplayCard = function(card) {
-  if(card.isAce() || card.number()==2) return true;
-  return (this.numCardsOnFoundations(card.altcolour(),card.number()-1) == 2);
-};
-
-
-
-///////////////////////////////////////////////////////////
-//// winning, scoring, undo
-Klondike.hasBeenWon = function() {
-  // game won if all 4 Foundations have 13 cards
-  for(var i = 0; i < 4; i++)
-    if(this.foundations[i].childNodes.length!=13)
-      return false;
-  return true;
-};
-Klondike.getScoreForAction = function(action) {
-  return (
-    action=="move-to-foundation"   ?   10 :
-    action=="move-from-waste"      ?    5 :
-    action=="card-revealed"        ?    5 :
-    action=="move-from-foundation" ?  -15 :
-    action=="stock-turned-over"    ? -100 :
-    0);
-};
-
-Games["Klondike"] = Klondike;
--- yukon.js
+++ yukon.js
@@ -1,115 +1,109 @@
-var Yukon = new CardGame();
+Games["Yukon"] = {
+  __proto__: BaseCardGame,
 
-Yukon.shortname = "yukon";
-
-
-
-///////////////////////////////////////////////////////////
-//// start game
-Yukon.deal = function() {
-  var cards = this.shuffleDecks(1);
-  this.dealToStack(cards,this.stacks[0],0,1);
-  for(var i = 1; i < 7; i++) this.dealToStack(cards,this.stacks[i],i,5);
-};
+  shortname: "yukon",
+  name: "Yukon",
 
 
-
-///////////////////////////////////////////////////////////
-//// Moving
-Yukon.canMoveToPile = function(card, stack) {
-  var lc = stack.lastChild; // last card
-  if(!lc) return true; // !lc && card.isKing()
-  // or last card (lc) in stack opposite colour and 1 greater than dragged card,
-  return (lc.faceUp() && lc.notSameColour(card) && lc.isConsecutiveTo(card));
-};
+  ///////////////////////////////////////////////////////////
+  //// start game
+  deal: function() {
+    var cards = this.shuffleDecks(1);
+    this.dealToStack(cards,this.stacks[0],0,1);
+    for(var i = 1; i < 7; i++) this.dealToStack(cards,this.stacks[i],i,5);
+  },
 
 
+  ///////////////////////////////////////////////////////////
+  //// Moving
+  canMoveToPile: function(card, stack) {
+    var lc = stack.lastChild; // last card
+    if(!lc) return true; // !lc && card.isKing()
+    // or last card (lc) in stack opposite colour and 1 greater than dragged card,
+    return (lc.faceUp() && lc.notSameColour(card) && lc.isConsecutiveTo(card));
+  },
 
-///////////////////////////////////////////////////////////
-//// Hints
-Yukon.getHints = function() {
-  // hints in Yukon are weird.  we look at the last card on each stack for targets, then go and find
-  // cards which could be moved there. (this is because any faceUp card can be moved in Yukon)
-  for(var i = 0; i < 7; i++) {
-    this.getHintsForCard(this.stacks[i].lastChild);
-  }
-};
-// take a card a find another card on a differnt stack of opposite colour and one less in rank
-Yukon.getHintsForCard = function(card) {
-  if(!card) return;
-  for(var i = 0; i < 7; i++) {
-    var stack = this.stacks[i];
-    if(stack==card.parentNode) continue;
-    var current = stack.lastChild;
-    while(current && current.faceUp()) {
-      if(card.isConsecutiveTo(current) && card.notSameColour(current)) {
-        // |current| could be moved onto |card|.  test if it's not already
-        // on a card consecutive and of opposite colour
-        var prev = current.previousSibling;
-        if(!prev || !prev.isConsecutiveTo(current) || !prev.notSameColour(current))
-          this.addHint(current,card.parentNode);
-      }
-      current = current.previousSibling;
+
+  ///////////////////////////////////////////////////////////
+  //// Hints
+  getHints: function() {
+    // hints in Yukon are weird.  we look at the last card on each stack for targets, then go and find
+    // cards which could be moved there. (this is because any faceUp card can be moved in Yukon)
+    for(var i = 0; i < 7; i++) {
+      this.getHintsForCard(this.stacks[i].lastChild);
     }
+  },
+  // take a card a find another card on a differnt stack of opposite colour and one less in rank
+  getHintsForCard: function(card) {
+    if(!card) return;
+    for(var i = 0; i < 7; i++) {
+      var stack = this.stacks[i];
+      if(stack==card.parentNode) continue;
+      var current = stack.lastChild;
+      while(current && current.faceUp()) {
+        if(card.isConsecutiveTo(current) && card.notSameColour(current)) {
+          // |current| could be moved onto |card|.  test if it's not already
+          // on a card consecutive and of opposite colour
+          var prev = current.previousSibling;
+          if(!prev || !prev.isConsecutiveTo(current) || !prev.notSameColour(current))
+            this.addHint(current,card.parentNode);
+        }
+        current = current.previousSibling;
+      }
+    }
+  },
+
+
+  ///////////////////////////////////////////////////////////
+  //// smart move
+  // finds the first stack from the left where the card can be moved to
+  getBestMoveForCard: function(card) {
+    var i, stack;
+    for(i = 0; i < 7; i++) {
+      stack = this.stacks[i];
+      if(stack!=card.parentNode && stack.hasChildNodes() && this.canMoveTo(card,stack)) return stack;
+    }
+    for(i = 0; i < 7; i++) {
+      stack = this.stacks[i];
+      if(stack!=card.parentNode && this.canMoveTo(card,stack)) return stack;
+    }
+    return null;
+  },
+
+
+  ///////////////////////////////////////////////////////////
+  //// Autoplay
+  autoplayMove: function() {
+    // move stuff to foundations
+    for(var i = 0; i < this.stacks.length; i++) {
+      var last = this.stacks[i].lastChild;
+      if(last && this.canMoveCard(last) && this.canAutoplayCard(last)
+        && this.sendToFoundations(last)) return true;
+    }
+    return false;
+  },
+
+  // card can be autoplayed if both cards with the next lower number and of opposite colour are on foundations
+  canAutoplayCard: function(card) {
+    if(card.isAce()) return true;
+    return (this.numCardsOnFoundations(card.altcolour(),card.number()-1) == 2);
+  },
+
+
+  ///////////////////////////////////////////////////////////
+  //// winning, scoring, undo
+  hasBeenWon: function() {
+    // game won if all 4 Foundations have 13 cards
+    for(var i = 0; i < 4; i++)
+      if(this.foundations[i].childNodes.length!=13)
+        return false;
+    return true;
+  },
+  getScoreForAction: function(action) {
+    return (
+      action=="move-to-foundation"   ?  10 :
+      action=="card-revealed"        ?   5 :
+      action=="move-from-foundation" ? -15 :
+      0);
   }
-};
-
-
-
-///////////////////////////////////////////////////////////
-//// smart move
-// finds the first stack from the left where the card can be moved to
-Yukon.getBestMoveForCard = function(card) {
-  var i, stack;
-  for(i = 0; i < 7; i++) {
-    stack = this.stacks[i];
-    if(stack!=card.parentNode && stack.hasChildNodes() && this.canMoveTo(card,stack)) return stack;
-  }
-  for(i = 0; i < 7; i++) {
-    stack = this.stacks[i];
-    if(stack!=card.parentNode && this.canMoveTo(card,stack)) return stack;
-  }
-  return null;
-};
-
-
-
-///////////////////////////////////////////////////////////
-//// Autoplay
-Yukon.autoplayMove = function() {
-  // move stuff to foundations
-  for(var i = 0; i < this.stacks.length; i++) {
-    var last = this.stacks[i].lastChild;
-    if(last && this.canMoveCard(last) && this.canAutoplayCard(last)
-      && this.sendToFoundations(last)) return true;
-  }
-  return false;
-};
-
-// card can be autoplayed if both cards with the next lower number and of opposite colour are on foundations
-Yukon.canAutoplayCard = function(card) {
-  if(card.isAce()) return true;
-  return (this.numCardsOnFoundations(card.altcolour(),card.number()-1) == 2);
-};
-
-
-
-///////////////////////////////////////////////////////////
-//// winning, scoring, undo
-Yukon.hasBeenWon = function() {
-  // game won if all 4 Foundations have 13 cards
-  for(var i = 0; i < 4; i++)
-    if(this.foundations[i].childNodes.length!=13)
-      return false;
-  return true;
-};
-Yukon.getScoreForAction = function(action) {
-  return (
-    action=="move-to-foundation"   ?  10 :
-    action=="card-revealed"        ?   5 :
-    action=="move-from-foundation" ? -15 :
-    0);
-};
-
-
-Games["Yukon"] = Yukon;
+}

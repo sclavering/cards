14e286d45f173946372c59befed198c1775fe9dd
Stephen Clavering <stephen@clavering.me.uk>
2004-05-16 18:55:48 +0100

    make undoing a Redeal work properly

--- montana.js
+++ montana.js
@@ -66,64 +66,58 @@ Games["montana"] = {
   //// redeal
   redeal: function() {
     this.redealsRemaining--;
-    // for redeal
-    var cards = [];
-    var runlengths = new Array(4);
-    // so we can undo the redeal later
-    var map = {};
-    map.starts = new Array(4);
-    map.cards = new Array(4);
+    var cards = []; 
+    var map = new Array(4);
+    var r, s, end; // r=row, s=stacknum
     // remove cards
-    for(var r = 0, i = 0; r < 4; r++, i = 13 * r) {
+    for(r = 0, s = 0; r < 4; r++, s = 13 * r) {
       var c = 0;
-      while(this.isPileComplete(i) && c<13) i++, c++;
-      map.starts[r] = runlengths[r] = c;
-      map.cards[r] = new Array(13 - c);
-      this.removeCards(i, 13 - c, cards, map.cards[r]);
+      while(this.isPileComplete(s) && c<13) s++, c++;
+      map[r] = new Array(13 - c);
+      for(var j = 0; j < 13 - c; j++) {
+        var card = this.stacks[s+j].firstChild;
+        if(card) {
+          card.parentNode.removeChild(card);
+          cards.push(card);
+        }
+        map[r][j] = card;
+      }
     }
     // shuffle
     cards = this.shuffle(cards);
     // deal
-    for(var row = 0; row < 4; row++) {
-      var start = 13 * row + runlengths[row] + 1;
-      var end = 13 * (row + 1);
-      for(var j = start; j < end; j++) this.dealToStack(cards, this.stacks[j], 0, 1);
+    for(r = 0, end = 13; r < 4; r++, end+=13) {
+      var start = end - map[r].length + 1;
+      for(s = start; s < end; s++) this.dealToStack(cards, this.stacks[s], 0, 1);
     }
     // track
     this.trackRedeal(map);
   },
-  isPileComplete: function(pilenum) {
-    var pile = this.stacks[pilenum];
-    var card = pile.firstChild;
+  isPileComplete: function(num) {
+    var pile = this.stacks[num];
+    var card = pile.lastChild;
     if(!card) return (pile.col==12);
     if(pile.col==0) return (card.number()==2);
-    var prvcard = this.stacks[pilenum-1].firstChild;
+    var prvcard = this.stacks[num-1].lastChild;
     return (card.isSameSuit(prvcard) && card.isConsecutiveTo(prvcard));
-  },
-  removeCards: function(start, num, cards, map) {
-    for(var i = 0; i < num; i++) {
-      var card = this.stacks[start+i].firstChild;
-      if(card) {
-        card.parentNode.removeChild(card);
-        cards.push(card);
-        map[i] = card;
-      } else {
-        map[i] = null;
-      }
-    }
   },
 
   undoRedeal: function(map) {
     this.redealsRemaining++;
-
-    for(var i = 0; i < 4; i++) {
-      var rowStart = 13*i;
-      for(var j = map.starts[i]; j < 13; j++) {
-        var stack = this.stacks[rowStart+j];
-        // remove anything in the way
-        while(stack.hasChildNodes()) stack.removeChild(stack.lastChild);
-        // restore the old card, if any
-        if(map.cards[i][j]) stack.addCard(map.cards[i][j]);
+    var r, j;
+    // remove whatever's there now
+    for(r = 0; r < 4; r++) {
+      var start = 13 * r;
+      for(j = 13 - map[r].length; j < 13; j++) {
+        var stack = this.stacks[start+j];
+        if(stack.hasChildNodes()) stack.removeChild(stack.lastChild);
+      }
+    }
+    // restore layout from before the redeal
+    for(r = 0; r < 4; r++) {
+      var offset = 13 * (r + 1) - map[r].length;
+      for(j = 0; j < map[r].length; j++) {
+        if(map[r][j]) this.stacks[offset+j].addCard(map[r][j]);
       }
     }
   },

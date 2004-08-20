5820883128d90b7a8828dcecef2e070c784b1f1f
Stephen Clavering <stephen@clavering.me.uk>
2004-08-20 15:53:29 +0100

    unbreak very short moves + simplify

--- moving.js
+++ moving.js
@@ -36,37 +36,36 @@ var CardMover1 = {
     var dx = tx - sx;
     var dy = ty - sy;
 
-    // otherwise we'll end up doing an inifinite step move
-    if(!dx && !dy) {
-      firstCard.transferTo(target);
+    // we'd like to move 55px diagonally per step, but have to settle for the closest distance that
+    // allows a *whole number* of steps, each of the same size
+    var steps = Math.round(Math.sqrt(dx*dx + dy*dy) / 55);
+//    var steps = (dx && dy) ? Math.round(Math.sqrt(dx*dx + dy*dy)) : (dx || dy);
+
+    if(steps==1 || steps==0) {
+      target.addCards(firstCard);
       animatedActionFinished();
       return;
     }
+
+    var stepX = dx / steps;
+    var stepY = dy / steps;
+    steps--; // so it's 0 when the move is complete
 
     // put cards in the temp pile. _top and _left properties remain as numbers, unlike top and left
     var cards = this.cards; // so we can use it in the nested functions
 //    cards.className = firstCard.parentNode.className; // so cards layed out as in originating pile
     cards.left = cards._left = sx;
     cards.top = cards._top = sy;
-    firstCard.transferTo(cards);
-
-    // we'd like to move 55px diagonally per step, but have to settle for the closest distance that
-    // allows a *whole number* of steps, each of the same size
-    var angle = Math.atan2(dy, dx);
-    var steps = Math.round((dx ? dx/Math.cos(angle) : dy/Math.sin(angle)) / 55);
-    var stepX = dx / steps;
-    var stepY = dy / steps;
-    steps--; // so it's 0 when the move is complete
+    cards.addCards(firstCard);
 
     var interval = null;
 
     // We want the cards to be displayed over their destination while the transfer happens.  Without
     // this function (called via a timer) that doesn't happen.
     function done() {
-      cards.firstChild.transferTo(target);
-      if(Game.autoplay()) return;
-      cards.hide();
-      enableUI();
+      target.addCards(firstCard);
+      cards.hide(); // must do this (even if it's about to be used for another move) to force repainting of area
+      animatedActionFinished();
     };
 
     function step() {

d20fda33bd9663e4926ae798448fd9eb2a9f5a71
Stephen Clavering <stephen@clavering.me.uk>
2004-08-29 12:24:07 +0100

    efficiency++?

--- fan.js
+++ fan.js
@@ -1,23 +1,33 @@
-Games["fan"] = {
+GamesMenu.fan =
+Games.fan = {
   __proto__: BaseCardGame,
 
   id: "fan",
   canMoveCard: "last on pile",
   canMoveToPile: "descending, in suit, kings in spaces",
 
+  init: function() {
+    var cards = this.cards = getDecks(1);
+
+    // add pointers to next card up and down in same suit
+    for(var i = 0; i != 51; i++) cards[i].up = cards[i+1];
+    for(i = 1; i != 52; i++) cards[i].down = cards[i-1];
+
+    cards[12].up = cards[25].up = cards[38].up = cards[51].up = null;
+    cards[0].down = cards[13].down = cards[26].down = cards[39].down = null;
+  },
+
   deal: function(cards) {
     for(var i = 0; i != 17; i++) dealToPile(cards, this.piles[i], 0, 3);
     dealToPile(cards, this.piles[17], 0, 1);
   },
 
-  // games with piles such as 7,2,6H or 4,9,8C are impossible
   shuffleImpossible: function(cards) {
     for(var p = 49; p != 1; p -= 3) {
       // these will form a pile c,d,e with c at the bottom
       var c = cards[p+2], d = cards[p+1], e = cards[p];
-      if(c.isSameSuit(d) && c.isSameSuit(e)
-          && ((c.isConsecutiveTo(e) && d.number<e.number)
-            ||(d.isConsecutiveTo(e) && c.number<e.number)))
+      // games with piles such as 7,2,6H or 4,9,8C are impossible
+      if(c.isSameSuit(d) && ((c==e.up && d.number<e.number) || (d==e.up && c.number<e.number)))
         return true;
     }
     return false;
@@ -26,21 +36,30 @@ Games["fan"] = {
   getHints: function() {
     for(var i = 0; i != this.piles.length; i++) {
       var card = this.piles[i].lastChild;
-      // don't suggest moving kings that are not on top of anything
-      if(!card || (card.isKing && !card.previousSibling)) continue;
-      var pile = searchPiles(this.piles, testCanMoveToPile(card));
-      if(pile) this.addHint(card, pile);
+      if(!card) continue;
+      var up = card.up;
+      if(up) { // not a King
+        if(!up.nextSibling) this.addHint(card, up.parentNode);
+      } else if(card.previousSibling) { // is a King, not in a space already
+        var pile = searchPiles(this.piles, testPileIsEmpty);
+        if(pile) this.addHint(card, pile);
+      }
     }
   },
 
   getBestMoveForCard: function(card) {
-    return searchPiles(this.piles, testCanMoveToPile(card));
+    var up = card.up;
+    if(!up) return searchPiles(this.piles, testPileIsEmpty);
+    return up.nextSibling ? null : up.parentNode;
   },
 
   autoplayMove: function() {
     for(var i = 0; i != this.piles.length; i++) {
-      var last = this.piles[i].lastChild;
-      if(last && this.sendToFoundations(last)) return true;
+      var card = this.piles[i].lastChild;
+      if(!card) continue;
+      if(!card.down) return this.moveTo(card, searchPiles(this.foundations, testPileIsEmpty));
+      var pile = card.down.parentNode;
+      if(pile.isFoundation) return this.moveTo(card, pile);
     }
     return false;
   },

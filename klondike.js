ca6a4fdbb19f34bb031733bba1f3214431f2609e
Stephen Clavering <stephen@clavering.me.uk>
2005-10-05 19:10:26 +0100

    Add Double Klondike

--- klondike.js
+++ klondike.js
@@ -3,6 +3,8 @@ const KlondikeBase = {
 
   foundationType: KlondikeFoundation,
   pileType: KlondikePile,
+  stockType: StockDealToWasteOrRefill,
+  wasteLayout: BaseLayout,
 
   dealTemplate: { piles: [[0,1],[1,1],[2,1],[3,1],[4,1],[5,1],[6,1]] },
 
@@ -58,8 +60,6 @@ const KlondikeBase = {
 
 Games.klondike1 = {
   __proto__: KlondikeBase,
-  stockType: StockDealToWasteOrRefill,
-  wasteLayout: BaseLayout,
   layoutTemplate: "v[1[sl]1w1#1f1f1f1f1] [1p1p1p1p1p1p1p1]"
 };
 
@@ -70,4 +70,17 @@ Games.klondike3 = {
   wasteType: Deal3Waste,
   wasteLayout: null,
   layoutTemplate: "v[1[sl]1w2f1f1f1f1] [1p1p1p1p1p1p1p1]"
+};
+
+
+Games.doubleklondike = {
+  __proto__: KlondikeBase,
+  init: function() {
+    const cs = this.cards = makeDecks(2);
+    this.foundationBases = [cs[0], cs[13], cs[26], cs[39], cs[52], cs[65], cs[78], cs[91]];
+  },
+  layoutTemplate: "v[1[sl]1w4f1f1f1f1f1f1f1f1] [1p1p1p1p1p1p1p1p1p1p1]",
+  dealTemplate: { piles: [[0,1],[1,1],[2,1],[3,1],[4,1],[5,1],[6,1],[7,1],[8,1],[9,1]] },
+  autoplay: "commonish 2deck",
+  getAutoplayableNumbers: "gypsy"
 };

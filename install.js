35050bb17cc05f0a646fec082750c2733450df52
Stephen Clavering <stephen@clavering.me.uk>
2007-05-11 00:03:36 +0100

    reorganise files, and add those not included in CVS import (locales, CSS, images, build stuff)

---
+++ install.js
@@ -0,0 +1,30 @@
+const kDisplayName = "Cards";
+const kName = "cards";
+const kPackage = "/cardgames.mozdev.org/cards";
+const kVersion = "0.99";
+
+const kJarFile = "cards.zip";
+const kContentFolder = "content/cards/";
+const kLocaleFolders  = ["en/"];
+const kSkinFolder = "";
+
+function die(err) {
+  alert("Install failed. Error code:" + err);
+  cancelInstall(err);
+}
+
+initInstall(kName, kPackage, kVersion);
+
+const folder = getFolder("Profile", "chrome");
+
+var err = addFile(kPackage, kVersion, kJarFile, folder, null)
+if(err == SUCCESS) {
+  var jar = getFolder(folder, kJarFile)
+  registerChrome(CONTENT | PROFILE_CHROME, jar, kContentFolder);
+  for(var i in kLocaleFolders) registerChrome(LOCALE | PROFILE_CHROME, jar, kLocaleFolders[i]);
+  if(kSkinFolder) registerChrome(SKIN | PROFILE_CHROME, jar, kSkinFolder);
+  err = performInstall();
+  if(err != SUCCESS && err != 999) die(err);
+} else {
+  die(err);
+}

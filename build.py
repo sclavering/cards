96c1892bc052358e7bc6e90c9f9375da94a8d49a
Stephen Clavering <stephen@clavering.me.uk>
2014-05-07 15:57:13 +0100

    include the JS and CSS inline cards.html

---
+++ build.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import os, sys
+
+JS_FILES = [
+    'util.js',
+    'main.js',
+    'lib-cards.js',
+    'lib-views.js',
+    'lib-layouts.js',
+    'lib-piles.js',
+    'lib-animation.js',
+    'autoplay.js',
+    'getAutoplayableNumbers.js',
+    'best_destination_for.js',
+    'lib-actions.js',
+    'lib-game.js',
+    'lib-freecell-move-sequencer.js',
+
+    'game-acesup.js',
+    'game-canfield.js',
+    'game-doublefan.js',
+    'game-doublesol.js',
+    'game-fan.js',
+    'game-fortythieves.js',
+    'game-freecell.js',
+    'game-golf.js',
+    'game-gypsy.js',
+    'game-klondike.js',
+    'game-maze.js',
+    'game-mod3.js',
+    'game-montana.js',
+    'game-penguin.js',
+    'game-pileon.js',
+    'game-pyramid.js',
+    'game-regiment.js',
+    'game-russiansol.js',
+    'game-spider.js',
+    'game-towers.js',
+    'game-tripeaks.js',
+    'game-unionsquare.js',
+    'game-whitehead.js',
+    'game-yukon.js',
+]
+
+
+path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/'
+
+
+js_code = ""
+for fn in JS_FILES:
+    js_code += "// ===== " + fn + " ===== \n\n"
+    js_code += open(path + 'js/' + fn).read()
+    js_code += "\n\n\n\n"
+js_code = '<script type="application/javascript;version=1.7">\n' + js_code + '</script>\n'
+
+css_code = "<style>\n" + open(path + 'cards.css').read() + "</style>\n"
+
+html_code = open(path + 'cards_raw.html').read()
+
+html_code = html_code.replace('###STYLE_AND_SCRIPT###', css_code + js_code)
+
+
+f = open(path + 'code/cards.html', 'w')
+f.write(html_code)

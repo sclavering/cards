d7f219549d2ac3fc500ab26053374faebae43ecf
Stephen Clavering <stephen@clavering.me.uk>
2015-08-09 22:56:36 +0100

    modernise the autoplay_default() code

--- src/js/autoplay.js
+++ src/js/autoplay.js
@@ -1,30 +1,23 @@
 function autoplay_default() {
-    // suit -> maximum number of that suit that can be autoplayed
-    const maxNums = this.getAutoplayableNumbers();
-    var triedToFillEmpty = false;
-    // numBs matters for Penguin
     const cs = this.allcards;
-    const ixs = this.foundationBaseIndexes, numIxs = ixs.length;
     // Try to put Aces (or whatever) on empty foundations.
     const empty = findEmpty(this.foundations); // used to check move legality
     if(empty) {
-      for(var j = 0; j !== numIxs; ++j) {
-        var b = cs[ixs[j]];
-        if(!b.pile.isFoundation && b.pile.mayTakeCard(b) && empty.mayAddCard(b))
-          return new Move(b, this.getFoundationForAce(b));
+      for(let ix of this.foundationBaseIndexes) {
+        let c = cs[ix];
+        if(!c.pile.isFoundation && c.mayTake && empty.mayAddCard(c)) return new Move(c, this.getFoundationForAce(c));
       }
     }
+    const maxNums = this.getAutoplayableNumbers();
     // Now try non-empty foundations
     for(let f of this.foundations) {
       if(!f.hasCards) continue;
-      var c = f.lastCard.up;
+      let c = f.lastCard.up;
       if(!c || c.number > maxNums[c.suit]) continue;
-      if(!c.pile.isFoundation && c.pile.mayTakeCard(c) && f.mayAddCard(c))
-        return new Move(c, f);
+      if(!c.pile.isFoundation && c.mayTake && f.mayAddCard(c)) return new Move(c, f);
       // for two-deck games
       c = c.twin;
-      if(c && !c.pile.isFoundation && c.pile.mayTakeCard(c) && f.mayAddCard(c))
-        return new Move(c, f);
+      if(c && !c.pile.isFoundation && c.mayTake && f.mayAddCard(c)) return new Move(c, f);
     }
     return null;
 };

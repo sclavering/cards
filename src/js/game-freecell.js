fe9885520772eb4f26efc81bc911d7c8727989ea
Stephen Clavering <stephen@clavering.me.uk>
2015-08-15 00:03:51 +0100

    game-freecell.js: add some commented-out code useful in testing animations

--- src/js/game-freecell.js
+++ src/js/game-freecell.js
@@ -1,6 +1,31 @@
 gGameClasses.freecell = {
   __proto__: FreeCellGame,
 
+  /* Uncomment some of this code to set up the cards for easy testing of freecell animations.
+  deal: function() {
+    // Move the 6H onto the 7C to test a medium move.
+    // this._deal_for_animation_testing([[12, 24, 10, 22, 8, 20, 6, 18, 4, 16, 2, 14]], 3, 5);
+    // Move the 8H onto the 9C to test a no-cells complex move.
+    // this._deal_for_animation_testing([[12, 24, 10, 22, 8, 20, 6, 18, 4, 16, 2, 14]], 4, 4);
+    // Move the 9S into a space to test a 1-cell complex move.
+    // this._deal_for_animation_testing([[12, 24, 10, 22, 8, 20, 6, 18, 4, 16, 2, 14]], 3, 4);
+    // Move the JS into a space to test another complex move.
+    // this._deal_for_animation_testing([[12, 24, 10, 22, 8, 20, 6, 18, 4, 16, 2, 14]], 2, 4);
+  },
+
+  _deal_for_animation_testing: function(card_indexes_list, num_cells_to_block, num_piles_to_block) {
+    for(let [i, ixs] of card_indexes_list.entries()) {
+      let cs = [for(ix of ixs) this.allcards[ix]];
+      this._deal_cards(cs, 0, this.piles[i], 0, cs.length);
+    }
+    const remaining = this.allcards.filter(c => !c.pile);
+    let to_block = this.cells.slice(0, num_cells_to_block);
+    if(num_piles_to_block) to_block = to_block.concat(this.piles.slice(-num_piles_to_block));
+    for(let p of to_block) this._deal_cards([remaining.pop()], 0, p, 0, 1);
+    this._deal_cards(remaining, 0, this.piles.slice(-1)[0], 0, remaining.length);
+  },
+  // */
+
   pileDetails: () => [
     "p", 8, FreeCellPile, FanDownView, 0, [7, 7, 7, 7, 6, 6, 6, 6],
     "f", 4, KlondikeFoundation, View, 0, 0,

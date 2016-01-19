2d23b67373fb43c0fa6588ccbdb57a57248b4356
Stephen Clavering <stephen@clavering.me.uk>
2016-01-19 00:28:42 +0000

    doublesol: remove the custom autoplay code
    It doesn't appear to be necessary.  autoplay_default should come up with the same moves, though by different means.

--- src/js/game-doublesol.js
+++ src/js/game-doublesol.js
@@ -18,30 +18,7 @@ gGameClasses.doublesol = {
 
   best_destination_for: find_destination__nearest_legal_pile,
 
-  autoplay: function() {
-    let triedToFillEmpty = false;
-    const cs = this.allcards;
-    const nums = this.autoplayable_numbers();
-
-    for(let f of this.foundations) {
-      if(f.hasCards) {
-        var last = f.getCard(-1), prv = f.getCard(-2);
-        var c1 = null, c2 = null;
-        if(prv === last.twin) c1 = last.up, c2 = prv.up;
-        else c1 = last.twin;
-        if(!c1 || c1.number > nums[c1.suit]) continue;
-        if(c1.faceUp && c1.isLast) return new Move(c1, f);
-        if(c2 && c2.faceUp && c2.isLast) return new Move(c2, f);
-      } else if(!triedToFillEmpty) {
-        triedToFillEmpty = true;
-        for(let ix of this.foundationBaseIndexes) {
-          let a = cs[ix];
-          if(a.faceUp && !a.pile.isFoundation && !a.twin.pile.isFoundation && a.isLast) return new Move(a, f);
-        }
-      }
-    }
-    return null;
-  },
+  autoplay: autoplay_default,
 
   autoplayable_numbers: autoplay_any_where_all_lower_of_other_colour_are_on_foundations_and_also_any_two,
 };

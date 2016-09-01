3a5bba262a96cb4a66cd4a02057bf49acbd30633
Stephen Clavering <stephen@clavering.me.uk>
2016-09-01 21:23:57 +0100

    make it possible to load src/cards.html directly during dev
    Retain build.py (renamed to dist.py) for deployment to avoid HTTP cacheing woes.
    
    The symlink is necessary because I don't want tsc_output.js in src/ (where it would interfere with grepping), yet Firefox blocks <script> tags from loading from a parent dir (but accepts symlinks).

---
+++ dist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# The purpose of this script is to merge the .css and (generated) .js code into the .html for easier distribution.  Having them all in one file ensures that you always get matching versions, whereas with separate files you would have to take measures to avoid the browser's cache giving you non-matching ones.
+
+import os, sys
+
+path = os.path.dirname(os.path.abspath(sys.argv[0])) + '/'
+
+try:
+    os.mkdir(path + "dist")
+except OSError:
+    pass
+
+external_css = '<link rel="stylesheet" href="cards.css">'
+external_js = '<script src="symlink_to_tsc_output.js"></script>'
+inline_css = "<style>\n" + open(path + "src/cards.css").read() + "</style>\n"
+inline_js = "<script>\n" + open(path + "tsc_output.js").read() + "</script>\n"
+
+in_html = open(path + "src/cards.html").read()
+out_html = in_html.replace(external_css, inline_css).replace(external_js, inline_js)
+out_html_f = open(path + 'dist/cards.html', 'w')
+out_html_f.write(out_html)
+
+import shutil
+shutil.copy(path + "src/cards.png", path + "dist/")
+shutil.copy(path + "src/help.html", path + "dist/")

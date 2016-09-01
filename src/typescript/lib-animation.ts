531d1bdff1cfbc6ccd324dee5ff97d9ae59de66f
Stephen Clavering <stephen@clavering.me.uk>
2016-09-01 15:02:23 +0100

    lib-animation.ts: add more types

--- src/typescript/lib-animation.ts
+++ src/typescript/lib-animation.ts
@@ -5,10 +5,10 @@ interface AnimationDetails {
 }
 
 class g_animations {
-  static _active: boolean = false;
-  static _args: AnimationDetails;
-  static _onsuccess: () => void;
-  static _timeouts: number[];
+  private static _active: boolean = false;
+  private static _args: AnimationDetails;
+  private static _onsuccess: () => void;
+  private static _timeouts: number[];
 
   // args: {
   //   steps: [relative delay in ms, function],
@@ -28,13 +28,13 @@ class g_animations {
     this._timeouts.push(setTimeout(() => g_animations._finished(true), time));
   }
 
-  static cancel() {
+  static cancel(): void {
     if(!this._active) return;
     if(this._timeouts) for(let t of this._timeouts) clearTimeout(t);
     this._finished(false);
   }
 
-  static _finished(success) {
+  static _finished(success): void {
     this._active = false;
     g_floating_pile.hide();
     for(let p of this._args.piles_to_update) p.view.update();
@@ -60,7 +60,7 @@ function prepare_move_animation(card: Card, destination: AnyPile): AnimationDeta
   const x0 = r0.left, y0 = r0.top;
 
   const transition_duration_ms = g_floating_pile.get_transition_duration_ms(x0, y0, x1, y1);
-  const steps : AnimationSteps = [
+  const steps: AnimationSteps = [
     [0, function() { g_floating_pile.transition_from_to(x0, y0, x1, y1, transition_duration_ms); }],
     // The small extra delay also serves to briefly show the cards at their destination but still floating.  This makes animations look a little better if the pile is a flex-fan that will re-pack the cards once actually added.
     [transition_duration_ms + k_animation_delay, function() {
@@ -71,7 +71,7 @@ function prepare_move_animation(card: Card, destination: AnyPile): AnimationDeta
 }
 
 
-function show_hints(card, destinations) {
+function show_hints(card: Card, destinations: AnyPile[]): void {
   const view = card.pile.view;
   view.show_hint_source(card);
   const hint_cards = card.pile.cards.slice(card.index);
@@ -88,38 +88,38 @@ function show_hints(card, destinations) {
 // A <canvas/> used to show cards being dragged or animated.
 class g_floating_pile {
   static _canvas: HTMLCanvasElement;
-  static context: CanvasRenderingContext2D;
-  static _prev_card: Card;
+  private static _context: CanvasRenderingContext2D;
+  private static _prev_card: Card;
 
-  static init() {
+  static init(): void {
     this._canvas = document.createElement("canvas");
     this._canvas.style.position = "absolute";
     // So people don't accidentally right-click and then pick the "View Image" item that's first in Firefox's menu.
     this._canvas.oncontextmenu = function(ev) { return false; };
     document.body.appendChild(this._canvas);
     this.hide();
-    this.context = this._canvas.getContext("2d");
+    this._context = this._canvas.getContext("2d");
     this._prev_card = null;
   }
 
-  static bounding_rect() {
+  static bounding_rect(): ClientRect {
     return this._canvas.getBoundingClientRect();
   }
 
-  static hide() {
+  static hide(): void {
     g_floating_pile._canvas.style.transition = "";
     this.set_position(-1000, -1000);
     this._prev_card = null;
   }
 
-  static start_animation_or_drag(card) {
+  static start_animation_or_drag(card: Card): void {
     // If we're already dragging a card and are about to animate its drop onto a pile, we want the animation to run from the current location, not from the original pile.
     if(this._prev_card === card) return;
     this._prev_card = card;
 
     const pile = card.pile, view = pile.view;
     const cards_taken = pile.cards.slice(card.index);
-    view.draw_into(this.context, cards_taken, false);
+    view.draw_into(this._context, cards_taken, false);
     const coords = view.coords_of_card(card);
     const r = view.pixel_rect();
     this.set_position(r.left + coords.x, r.top + coords.y);
@@ -128,25 +128,25 @@ class g_floating_pile {
   }
 
   // It's always of just the top card
-  static start_freecell_animation(src, cards_remaining, moving_card, x, y) {
+  static start_freecell_animation(src: AnyPile, cards_remaining: Card[], moving_card: Card, x: number, y: number): void {
     const view = src.view;
-    view.draw_into(this.context, [moving_card], false);
+    view.draw_into(this._context, [moving_card], false);
     this.set_position(x, y);
     view.update_with(cards_remaining);
   }
 
-  static set_position(x, y) {
+  static set_position(x: number, y: number): void {
     this._canvas.style.left = x + "px";
     this._canvas.style.top = y + "px";
   }
 
-  static transition_from_to(x0, y0, x1, y1, duration_ms) {
+  static transition_from_to(x0: number, y0: number, x1: number, y1: number, duration_ms: number): void {
     this.set_position(x0, y0);
     this._canvas.style.transition = "left " + duration_ms + "ms, top " + duration_ms + "ms";
     this.set_position(x1, y1);
   }
 
-  static get_transition_duration_ms(x0, y0, x1, y1) {
+  static get_transition_duration_ms(x0: number, y0: number, x1: number, y1: number): number {
     const dx = x1 - x0, dy = y1 - y0;
     // We move 55px diagonally per k_animation_delay.  (This is left over from when animation was done without CSS transitions).
     return Math.round(Math.sqrt(dx * dx + dy * dy) / 55 * k_animation_delay);

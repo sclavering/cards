b09de49690011829d729351c0db9cee6de907916
Stephen Clavering <stephen@clavering.me.uk>
2016-09-01 12:18:23 +0100

    stop using Firefox-specific .originalTarget
    I imagine when I wrote this I was thinking of .target and .originalTarget as meaning what .currentTarget and .target (respectively) mean.

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -94,7 +94,7 @@ function help(): void {
 
 function onGameSelected(ev: Event): boolean {
   hideGameChooser();
-  const m = ev.originalTarget.id.match(/^choosegame-(.*)$/);
+  const m = ev.target.id.match(/^choosegame-(.*)$/);
   if(m) playGame(m[1]);
   return false;
 }
